d5062f29b8b085497a58da30bfd3daf0e77a5602
sonyokpara <sony.okpara@yahoo.com>
2024-06-07 14:33:23 +0000

    Add the structure for the Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,21 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="w-full flex flex-col xl:flex-row justify-center min-h-screen gap-10 max-container">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start pt-28 max-xl:padding-x w-full">
+        <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span> Shoes
+        </h1>
+        <p>
+          Discover the stylish Nike arrivals, quality, comfort, and innovation
+          for your active life.
+        </p>
+        Button
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
