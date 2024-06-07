9da27852861f7cef340a949c169529460921bee4
sonyokpara <sony.okpara@yahoo.com>
2024-06-07 12:29:07 +0000

    Add index.js import file to import all the section file into App.jsx

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+
+export {
+  Hero,
+  Footer,
+  Subscribe,
+  Services,
+  SuperQuality,
+  CustomerReviews,
+  SpecialOffer,
+  PopularProducts,
+};
