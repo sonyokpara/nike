125ef367be8b08b4644c8f084f810b1254479dce
sonyokpara <sony.okpara@yahoo.com>
2024-06-07 13:15:47 +0000

    Complete the header navigation menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,31 @@
+import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
+
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
-      <nav>
+      <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
+
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a
+                href={link.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
