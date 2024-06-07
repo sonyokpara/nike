4a613d9dc2997cecb1b6069e4a773babe05e4175
sonyokpara <sony.okpara@yahoo.com>
2024-06-07 12:59:32 +0000

    Add the contants folder for all dev contants

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import { headerLogo } from "../assets/images";
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav>
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
